e5baabd46d4fa6e45939b4aff0cc5c862360d982
austin <austinmandrade@gmail.com>
2022-10-18 23:12:06 -0400

    Complete static home page with dynamic page rendering

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-export const Layout = () => {
+export const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>Wholesale Tech</title>
+      </Head>
+      <header>
+        <Navbar/>
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
   )
 }
+
+export default Layout
